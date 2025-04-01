1f5772875a4d0008aa92a6b02c62b71780c13d99
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2025-04-01 11:57:09 +0200

    add more javafx code

--- ui-app/src/main/java/at/technikum/uiapp/HelloController.java
+++ ui-app/src/main/java/at/technikum/uiapp/HelloController.java
@@ -2,13 +2,28 @@ package at.technikum.uiapp;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+
+    private int counter = 0;
+
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextField numberA;
+
+    @FXML
+    private TextField numberB;
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        int a = Integer.parseInt(numberA.getText());
+        int b = Integer.parseInt(numberB.getText());
+
+        counter++;
+        welcomeText.setText(String.format("%d + %d = %d", a, b, a + b));
+        // nameInput.setText("");
     }
 }
