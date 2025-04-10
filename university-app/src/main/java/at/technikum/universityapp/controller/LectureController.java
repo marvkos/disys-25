ad75b6b5ea7f047e8634043d855e54d0e054b62d
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2025-04-10 14:23:51 +0200

    add additional endpoints

--- university-app/src/main/java/at/technikum/universityapp/controller/LectureController.java
+++ university-app/src/main/java/at/technikum/universityapp/controller/LectureController.java
@@ -3,11 +3,13 @@ package at.technikum.universityapp.controller;
 import at.technikum.universityapp.entity.Lecture;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class LectureController {
@@ -19,11 +21,20 @@ public class LectureController {
 
     @GetMapping("/lectures")
     public List<Lecture> all(@RequestParam LocalDateTime start) {
-        
-
         return List.of(
                 new Lecture("DISYS", "mkosmider", 5),
                 new Lecture("SLT", "mkosmider", 2)
         );
+    }
+
+    @GetMapping("/lectures/{id}")
+    public Lecture read(@PathVariable int id) {
+        return new Lecture("DISYS", "mkosmider", 5);
+    }
+
+    @GetMapping("/lectures/count")
+    public int count() {
+        Random random = new Random();
+        return random.nextInt(100);
     }
 }
